2f5d689bd99ae424fdbf27dae989d735b9b1480d
Ilya Ivanchikov <phantomonster1@mail.ru>
2019-08-22 00:09:13 +0200

    feat: my server took off

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,51 @@
-module.exports = function(app, db) {
-    app.post('/notes', (req, res) => {
-        console.log(req.body);
-        res.send('Hello');
+const ObjectID = require('mongodb').ObjectID;
+  module.exports = function(app, db) {
+    app.get('/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      database = db.db('notes');
+      database.collection('notes').findOne(details, (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send(item);
+        } 
+      });
     });
-};
+    app.put ('/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      const note = { text: req.body.body, title: req.body.title };
+      database = db.db('notes');
+      database.collection('notes').update(details, note, (err, result) => {
+        if (err) {
+            res.send({'error':'An error has occurred'});
+        } else {
+            res.send(note);
+        } 
+      });
+    });
+    app.delete('/notes/:id', (req, res) => {
+      const id = req.params.id;
+      const details = { '_id': new ObjectID(id) };
+      database = db.db('notes');
+      database.collection('notes').remove(details, (err, item) => {
+        if (err) {
+          res.send({'error':'An error has occurred'});
+        } else {
+          res.send('Note ' + id + ' deleted!');
+        } 
+      });
+    });
+  app.post('/notes', (req, res) => {
+      const note = { text: req.body.body, title: req.body.title };
+      database = db.db('notes');
+      database.collection('notes').insert(note, (err, result) => {
+        if (err) { 
+          res.send({ 'error': 'An error has occurred' }); 
+        } else {
+          res.send(result.ops[0]);
+        }
+      });
+    });
+  };
